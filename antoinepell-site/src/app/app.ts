2ec25012c3e1e9f154518957df21f30410ff9e7f
Antoine Pellissier <antoine.pellissier0@gmail.com>
2025-06-09 17:13:30 +0200

    V1.1 ok

--- antoinepell-site/src/app/app.ts
+++ antoinepell-site/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GlobalHeader } from './components/global-header/global-header';
-import { WelcomePage } from './components/welcome-page/welcome-page';
-import { ProjectPreviewList } from './components/project-preview-list/project-preview-list';
 
 @Component({
   selector: 'app-root',
-  imports: [GlobalHeader, RouterOutlet, WelcomePage, ProjectPreviewList],
+  imports: [GlobalHeader, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
